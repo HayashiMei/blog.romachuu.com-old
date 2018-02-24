e03eafbe19daf3ff7a2492e2926e0b9e500eb4aa
root <root@hotaru.c.megumi-172207.internal>
2018-02-24 05:54:12 +0000

    Site updated: 2018-02-24 05:54:12

--- js/src/title.js
+++ js/src/title.js
@@ -1,14 +1,28 @@
-var OriginTitile = document.title;
-var titleTime;
-document.addEventListener('visibilitychange', function() {
-    if (document.hidden) {
-        document.title = '(つェ⊂)我藏好了哦~ ' + OriginTitile;
-        clearTimeout(titleTime);
+if (window.CONFIG) {
+    window.CONFIG.favicon = {
+        visibilitychange: true,
+        hidden: '/images/failure.ico',
+        hide_text: '(つェ⊂) 我藏好了哦~ ',
+        show: '/images/favicon.png',
+        show_text: '(*´∇｀*) 被你发现啦~ '
     }
-    else {
-        document.title = '(*´∇｀*) 被你发现啦~ ' + OriginTitile;
-        titleTime = setTimeout(function() {
-            document.title = OriginTitile;
-        }, 2000);
-    }
-});
+}
+
+if (window.CONFIG.favicon.visibilitychange) {
+    var OriginTitile = document.title
+    var titleTime
+    document.addEventListener('visibilitychange', function () {
+        if (document.hidden) {
+            $('[rel="icon"]').attr('href', window.CONFIG.favicon.hidden)
+            document.title = window.CONFIG.favicon.hide_text
+            clearTimeout(titleTime)
+        }
+        else {
+            $('[rel="icon"]').attr('href', window.CONFIG.favicon.show)
+            document.title = window.CONFIG.favicon.show_text
+            titleTime = setTimeout(function () {
+                document.title = OriginTitile
+            }, 2000)
+        }
+    })
+}
